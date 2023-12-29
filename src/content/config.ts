254dc05b6a3daa3c895583df9f0c67d943666dfc
shomrkm <shochan1985@gmail.com>
2023-12-29 11:45:21 +0900

    feat: zenn-markdown を導入

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,6 @@ import { z, defineCollection } from 'astro:content'
 const productCollection = defineCollection({
   type: 'content',
   schema: z.object({
-    id: z.string(),
     name: z.string(),
     description: z.string(),
     price: z.number(),
